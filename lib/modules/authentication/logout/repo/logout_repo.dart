b6b7739a45bf8fcab1c265c8e5ebf101f49beae2
lowfitz <g.selim1970@gmail.com>
2024-02-25 18:44:13 +0200

    new

--- lib/modules/authentication/logout/repo/logout_repo.dart
+++ lib/modules/authentication/logout/repo/logout_repo.dart
@@ -1,12 +1,11 @@
 import 'package:new_bussiness_app/General/end_points.dart';
+import 'package:new_bussiness_app/General/utils.dart';
 import 'package:new_bussiness_app/data/remote/dio_service.dart';
 
 class LogoutRepo {
   static Future logoutrepo() async {
     final response = await DioService.postData(
-      url: EndPoints.logout,
-      loading: true,
-    );
+        url: EndPoints.logout, loading: true, token: Utils.token);
     print(response?.statusCode);
     return response?.data;
   }
